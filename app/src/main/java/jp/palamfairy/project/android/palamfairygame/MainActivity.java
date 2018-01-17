5bb7b4685385faaa838ce0ac0ff8ee5637fb811f
youko-wakou <appu2pudding@gmail.com>
2018-01-18 04:03:14 +0900

    bgm

--- app/src/main/java/jp/palamfairy/project/android/palamfairygame/MainActivity.java
+++ app/src/main/java/jp/palamfairy/project/android/palamfairygame/MainActivity.java
@@ -1,12 +1,15 @@
 package jp.palamfairy.project.android.palamfairygame;
 
+import android.media.AudioManager;
 import android.media.MediaActionSound;
+import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView petImage;
     private TranslateAnimation petMoveRight;
     private TranslateAnimation petMoveLeft;
-    private SoundPool mBgm;
+    private MediaPlayer mBgm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +28,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
-        sound();
-        for(int i = 0;i<1000;i++) {
-//        右に動く
-            ImageView petImage = (ImageView) findViewById(R.id.petViewImage);
-            TranslateAnimation petMoveRight = new TranslateAnimation(0, 100, 0, 0);
-            petMoveRight.setDuration(1000);
-            petImage.startAnimation(petMoveRight);
-//        左に動く
-            TranslateAnimation petMoveLeft = new TranslateAnimation(0, -100, 0, 0);
-            petMoveRight.setDuration(1000);
-            petImage.startAnimation(petMoveLeft);
-        }
+
+
 //        ボタン
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -46,6 +39,30 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        sound();
+        Log.d("test", "あああああああ");
+        for (int i = 0; i < 1000; i++) {
+//        右に動く
+            Log.d("test", "aaaaaaaaaaa");
+            ImageView petImage = (ImageView) findViewById(R.id.petViewImage);
+            TranslateAnimation petMoveRight = new TranslateAnimation(
+                    TranslateAnimation.RELATIVE_TO_SELF, 0,
+                    TranslateAnimation.ABSOLUTE, 1000,
+                    TranslateAnimation.RELATIVE_TO_SELF, 0,
+                    TranslateAnimation.RELATIVE_TO_SELF, 0
+            );
+            petMoveRight.setDuration(5000);
+            petImage.startAnimation(petMoveRight);
+//        左に動く
+//            TranslateAnimation petMoveLeft = new TranslateAnimation(0, -100, 0, 0);
+//            petMoveRight.setDuration(1000);
+//            petImage.startAnimation(petMoveLeft);
+        }
     }
 
 //    @Override
@@ -69,8 +86,10 @@ public class MainActivity extends AppCompatActivity {
 //
 //        return super.onOptionsItemSelected(item);
 //    }
-    public void sound(){
-        int soundID =mBgm.load("@ras/bgm",1);
-        mBgm.play(soundID,1.0f,1.0f,1,0,1.0f);
+
+    public void sound() {
+        mBgm = MediaPlayer.create(this, R.raw.bgm);
+        mBgm.start();
     }
+
 }
